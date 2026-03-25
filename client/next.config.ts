d951b9c5b812a8fb2e988a2d6edaf87e309ab3eb
sambit2804 <sambit.official2006@gmail.com>
2026-03-25 13:36:59 +0530

    Update from StellarIDE - 2026-03-25T08:06:58.989Z

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: ['svc-535fcdfd-morphvm-nob96fli.http.cloud.morph.so']
 };
 
 export default nextConfig;
